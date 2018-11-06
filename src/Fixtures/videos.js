24fa298fbe905325dfbf6f470c638a3f2c893654
Francis Breidenbach <fbreidenbach@gmail.com>
2018-11-05 21:08:03 -0800

    Add video playlist fixture

---
+++ src/Fixtures/videos.js
@@ -0,0 +1,16 @@
+export const playlist = {
+  videos: [
+    {
+      service: "vimeo",
+      video: "https://vimeo.com/295913617"
+    },
+    {
+      service: "vimeo",
+      video: "https://vimeo.com/297764640"
+    },
+    {
+      service: "dailymotion",
+      video: "https://vimeo.com/295913617"
+    }
+  ]
+};
