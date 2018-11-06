ca343ff902b8b521f3583fc9b6daec2db3c19753
Francis Breidenbach <fbreidenbach@gmail.com>
2018-11-05 22:47:12 -0800

    Refactor playlist to array

--- src/Fixtures/videos.js
+++ src/Fixtures/videos.js
@@ -1,16 +1,14 @@
-export const playlist = {
-  videos: [
-    {
-      service: "vimeo",
-      video: "https://vimeo.com/295913617"
-    },
-    {
-      service: "vimeo",
-      video: "https://vimeo.com/297764640"
-    },
-    {
-      service: "dailymotion",
-      video: "https://vimeo.com/295913617"
-    }
-  ]
-};
+export const playlist = [
+  {
+    service: "vimeo",
+    video: "https://vimeo.com/295913617"
+  },
+  {
+    service: "vimeo",
+    video: "https://vimeo.com/297764640"
+  },
+  {
+    service: "vimeo",
+    video: "https://vimeo.com/295913617"
+  }
+];
