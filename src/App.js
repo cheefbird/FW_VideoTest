ae225c120a00226f3d827f368bb885e61ded21c2
Francis Breidenbach <fbreidenbach@gmail.com>
2018-11-06 19:11:07 -0800

    Shortcut implementation for POC test.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, WebView } from "react-native";
+
+import VideoPlayerScreen from "./Screens/VideoPlayerScreen";
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <View style={styles.middleContainer}>
+          <WebView
+            source={{ uri: "https://player.vimeo.com/video/252213768" }}
+            allowsInlineMediaPlayback={true}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>TEXT GOES HERE</Text>
+        </View>
       </View>
     );
   }
@@ -16,17 +25,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    alignItems: "stretch",
+    backgroundColor: "#000"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  middleContainer: {
+    height: 233,
+    backgroundColor: "red"
   },
-  instructions: {
+  textContainer: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    marginHorizontal: 18,
+    marginTop: 20
+  },
+  text: {
     textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+    color: "red",
+    fontSize: 18,
+    fontWeight: "bold"
   }
 });
