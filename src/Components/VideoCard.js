8913649173b534d12e48c807f8bc2124e86b7a57
Francis Breidenbach <fbreidenbach@gmail.com>
2018-11-06 22:57:24 -0800

    Create and display VideoCard and WatchButton on home screen.

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, ImageBackground, Button } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
+import { StyleSheet, View, Text, ImageBackground } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import WatchButton from "./WatchButton";
 
 export default class VideoCard extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <ImageBackground
-          source={{ uri: "https://via.placeholder.com/414x233/0000FF/0000FF" }}
+          style={styles.thumbnail}
+          source={{ uri: "https://via.placeholder.com/368x207/0000FF/0000FF" }}
         >
-          <Icon name="arrow-dropright-circle" size={24} color="#d3d6dd" />
+          <Icon
+            name="play-circle"
+            size={75}
+            color="#d3d6dd"
+            style={{ alignItems: "center" }}
+          />
         </ImageBackground>
-        <View>
-          <Text>Video's Title Here</Text>
-          <Button>Watch Video</Button>
+        <View style={styles.textContainer}>
+          <Text style={styles.videoTitle}>Video Title Here</Text>
+          <WatchButton />
         </View>
       </View>
     );
@@ -21,9 +29,25 @@ export default class VideoCard extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: "white",
+    borderRadius: 8,
+    marginVertical: 4,
+    marginHorizontal: 10,
+    padding: 8
+  },
   thumbnail: {
-    width: 414,
-    height: 233
+    width: 368,
+    height: 207,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textContainer: {
+    alignItems: "stretch"
+  },
+  videoTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    paddingHorizontal: 5
   }
 });
---
+++ src/Components/WatchButton.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
+
+export default class WatchButton extends Component {
+  render() {
+    return (
+      <TouchableHighlight
+        style={styles.container}
+        activeOpacity={1}
+        underlayColor="green"
+        onPress={() => console.log("Button Pressed")}
+      >
+        <Text style={styles.text}>Watch Video</Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "red",
+    paddingVertical: 8,
+    paddingHorizontal: 18,
+    marginVertical: 5,
+    marginHorizontal: 12,
+    borderRadius: 10
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#000",
+    textAlign: "center"
+  }
+});
